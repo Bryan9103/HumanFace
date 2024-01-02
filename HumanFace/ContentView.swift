fd5c024e1d3f16d5d763cf7357907a48a2b135c9
B <bryanandersen03@gmail.com>
2024-01-03 01:32:07 +0800

    Initial Commit

---
+++ HumanFace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HumanFace
+//
+//  Created by Bryan Andersen on 2024/1/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HumanFace/HumanFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanFaceApp.swift
+//  HumanFace
+//
+//  Created by Bryan Andersen on 2024/1/3.
+//
+
+import SwiftUI
+
+@main
+struct HumanFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
